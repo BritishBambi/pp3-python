9be06c90be9dec4b43116e2f18e806e2869fb032
BritishBambi <josefjakubiak@gmail.com>
2022-01-11 16:23:07 +0000

    Implement play again function to be called on game over conditions

--- run.py
+++ run.py
@@ -9,5 +9,17 @@ def game_intro():
     print("This text based adventure game will test your skills...")
     time.sleep(2)
     print("Are you ready to face the zombie horde?")
+    play_again()
+
+def play_again():
+    """
+    Will allow the game to be ran again when and end condition is met"
+    """
+    play_again = input("Would you like to play again? yes/no:\n")
+    if play_again == "yes":
+        game_intro()
+    elif play_again == "no":
+        print("That's unfortunate, see you next time!")
+        
 
 game_intro()
